63f98152432441a1b819721c6f36b6c88c071f52
Omar <morenoomar29@yahoo.com>
2021-11-02 20:41:38 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,106 @@
 // Code your solution in this file!
+
+
+function distanceFromHqInBlocks(dist) { 
+    if (dist >= 42) {
+        return dist - 42;
+    } else (dist < 42)
+        return 42 - dist;
+    }
+    
+function distanceFromHqInFeet(feet) {
+        let i = distanceFromHqInBlocks(feet)
+            return (i*264)
+        }
+        
+function distanceTravelledInFeet(num1, num2) {
+        let blocks
+          if (num2 > num1) {
+            blocks = num2 - num1
+                return (blocks * 264)
+    }
+      if (num1 > num2) {
+        blocks = num1 - num2;
+           return (blocks * 264)  
+        }
+    }
+
+    function calculatesFarePrice(start, dest) {
+        let feet = distanceTravelledInFeet(start, dest) 
+        let fare
+        if (feet <= 400) {
+            fare = 0
+        } else if (feet > 400 && feet <= 2000){
+            fare = ((feet - 400)* 2) / 100
+        } else if (feet > 2000 && feet < 2500) {
+            fare = 25
+        } else {
+            fare = 'cannot travel that far'
+        } return fare
+       }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
